8d5c02e4a0b4448656855524a017f5cdfc0bdc7c
HarolC17 <salazarharol30@gmail.com>
2025-11-19 23:08:49 -0500

    Add CORS configuration for Vercel

--- src/main/java/com/perfumeria/auth/infrastructure/entry_points/UsuarioController.java
+++ src/main/java/com/perfumeria/auth/infrastructure/entry_points/UsuarioController.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/perfumeria/usuario")
-@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class UsuarioController {
 
